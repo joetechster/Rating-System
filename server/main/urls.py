0b257c21cbceefa4137a207a8cd688f3a0118254
joetechster <bamideledamilola3@gmail.com>
2024-07-27 23:31:09 +0100

    trial

--- server/main/urls.py
+++ server/main/urls.py
@@ -21,10 +21,8 @@ urlpatterns = [
     path('', views.student_signin)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-
-
-# urlpatterns += [
-#     re_path(r'^media/(?P<path>.*)$', serve, {
-#         'document_root': settings.MEDIA_ROOT,
-#     }),
-# ]
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
